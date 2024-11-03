f9f8b09f0abcb3226e5a7e69c9ed05cb7df1ce19
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-11-03 20:47:37 +0100

    create my first component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <WelcomeMessage/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <WelcomeMessage/>
+     
     </>
   )
 }
