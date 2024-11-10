cb1176c946c5cf04ad042a5208251ac8e74d0c67
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-11-10 21:57:50 +0100

    feature:adding context API to share props from the parents to the child

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import UserContext from "./components/UserContext";
+import ProfilePage from "./components/ProfilePage";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <UserContext.Povider value={userData}>
+          <>
+          <ProfilePage />
+          </>
+         
+        </UserContext.Povider>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        
+        <Footer />
+        
+        
+      </div>
+      
+     
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-props/src/components/UserContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserContext() {
+    const context = React.createContext();
+    return(context);
+}
+export default UserContext;
