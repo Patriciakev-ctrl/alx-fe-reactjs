2b237441c707d4530241f35a4ee2195ed9a482ed
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-11-10 20:45:39 +0100

    adding inline style to components

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 const UserProfile = (props) => {
     return(
-        <div style={{margin: '20px 10px', fontSize: '15px', border :'1px solid gray'}}>
-             <h2 style={{color: 'black'}}>{props.name}</h2>
-             <p style={{color: 'black'}}>Age: {props.age}</p>
-             <p style={{color: 'red'}}>Bio: {props.bio}</p>
-        </div>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h2 style={{ color: 'blue' }}>{props.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+            <p>Bio: {props.bio}</p>
+ </div>
 
     );
 };
