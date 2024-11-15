020317b68f22be3aff35131e54e535227dac1935
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-11-15 22:40:30 +0100

    Adding Vite to the package JSON

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,4 +1,7 @@
+
+import './App.css';
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Services   from "./components/Services";
 import  Home  from "./components/Home";
@@ -6,14 +9,32 @@ import Contact from "./components/Contact";
 import NavBar  from "./components/NavBar";
 import Footer from "./components/Footer";
 
-const App = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <Footer></Footer>
-            
-        </div>
-    );
-};
+
+function App() {
+  return (
+
+    <> 
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <main> 
+        <Routes>
+        
+          <Route index element={<Home/>} />
+          <Route path="services" element={<Services/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Routes>
+        </main>
+        <Footer></Footer>
+        
+
+      </BrowserRouter>
+    
+
+
+</>
+);
+
+}
 
 export default App;
